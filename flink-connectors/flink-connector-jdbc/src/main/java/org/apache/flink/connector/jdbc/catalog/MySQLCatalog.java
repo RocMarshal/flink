0c41b90ed866b6999bedc9602b8cbd2973388d97
Roc Marshal <flinker@126.com>
2021-07-12 18:54:17 +0800

    temprory archive

---
+++ flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/catalog/MySQLCatalog.java
@@ -0,0 +1,303 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.connector.jdbc.catalog;
+
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.constraints.UniqueConstraint;
+import org.apache.flink.table.catalog.CatalogBaseTable;
+import org.apache.flink.table.catalog.CatalogDatabase;
+import org.apache.flink.table.catalog.CatalogDatabaseImpl;
+import org.apache.flink.table.catalog.CatalogTableImpl;
+import org.apache.flink.table.catalog.ObjectPath;
+import org.apache.flink.table.catalog.exceptions.CatalogException;
+import org.apache.flink.table.catalog.exceptions.DatabaseNotExistException;
+import org.apache.flink.table.catalog.exceptions.TableNotExistException;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.util.Preconditions;
+
+import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.compress.utils.Sets;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.apache.flink.connector.jdbc.table.JdbcDynamicTableFactory.IDENTIFIER;
+import static org.apache.flink.connector.jdbc.table.JdbcDynamicTableFactory.PASSWORD;
+import static org.apache.flink.connector.jdbc.table.JdbcDynamicTableFactory.TABLE_NAME;
+import static org.apache.flink.connector.jdbc.table.JdbcDynamicTableFactory.URL;
+import static org.apache.flink.connector.jdbc.table.JdbcDynamicTableFactory.USERNAME;
+import static org.apache.flink.table.factories.FactoryUtil.CONNECTOR;
+
+public class MySQLCatalog extends AbstractJdbcCatalog {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MySQLCatalog.class);
+
+    private static final Set<String> builtinDatabases =
+            Sets.newHashSet("information_schema", "mysql", "performance_schema", "sys");
+
+    public static final String MYSQL_TINYINT = "TINYINT";
+    public static final String MYSQL_SMALLINT = "SMALLINT";
+    public static final String MYSQL_TINYINT_UNSIGNED = "TINYINT UNSIGNED";
+    public static final String MYSQL_INT = "INT";
+    public static final String MYSQL_MEDIUMINT = "MEDIUMINT";
+    public static final String MYSQL_SMALLINT_UNSIGNED = "SMALLINT UNSIGNED";
+    public static final String MYSQL_BIGINT = "BIGINT";
+    public static final String MYSQL_INT_UNSIGNED = "INT UNSIGNED";
+    public static final String MYSQL_BIGINT_UNSIGNED = "BIGINT UNSIGNED";
+    public static final String MYSQL_FLOAT = "FLOAT";
+    public static final String MYSQL_DOUBLE = "DOUBLE";
+    public static final String MYSQL_NUMERIC = "NUMERIC";
+    public static final String MYSQL_DECIMAL = "DECIMAL";
+    public static final String MYSQL_BOOLEAN = "BOOLEAN";
+    public static final String MYSQL_DATE = "DATE";
+    public static final String MYSQL_TIME = "TIME";
+    public static final String MYSQL_DATETIME = "DATETIME";
+    public static final String MYSQL_CHAR = "CHAR";
+    public static final String MYSQL_VARCHAR = "VARCHAR";
+    // mysql text 类型实质是 varchar(13618)
+    public static final String MYSQL_TEXT = "TEXT";
+
+    public MySQLCatalog(
+            String catalogName,
+            String defaultDatabase,
+            String username,
+            String pwd,
+            String baseUrl) {
+        super(catalogName, defaultDatabase, username, pwd, baseUrl);
+    }
+
+    @Override
+    public List<String> listDatabases() throws CatalogException {
+        List<String> mysqlDatabases = Lists.newArrayList();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try (Connection connection = DriverManager.getConnection(defaultUrl, username, pwd)) {
+            ps =
+                    connection.prepareStatement(
+                            "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA;");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String dbName = rs.getString(1);
+                if (!builtinDatabases.contains(dbName)) {
+                    mysqlDatabases.add(dbName);
+                }
+            }
+        } catch (Exception e) {
+            throw new CatalogException(
+                    String.format("Failed in listing database in catalog %s", getName()), e);
+        } finally {
+            release(rs);
+            release(ps);
+        }
+        return mysqlDatabases;
+    }
+
+    @Override
+    public CatalogDatabase getDatabase(String databaseName)
+            throws DatabaseNotExistException, CatalogException {
+        Preconditions.checkState(
+                StringUtils.isNotBlank(databaseName), "Database name must not be blank.");
+        if (listDatabases().contains(databaseName)) {
+            return new CatalogDatabaseImpl(Collections.emptyMap(), null);
+        } else {
+            throw new DatabaseNotExistException(getName(), databaseName);
+        }
+    }
+
+    @Override
+    public List<String> listTables(String databaseName)
+            throws DatabaseNotExistException, CatalogException {
+        Preconditions.checkState(
+                StringUtils.isNotBlank(databaseName), "Database name must not be blank.");
+        if (!databaseExists(databaseName)) {
+            throw new DatabaseNotExistException(getName(), databaseName);
+        }
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<String> tables = Lists.newArrayList();
+        // 获取 database 下所有表
+        try (Connection conn = DriverManager.getConnection(baseUrl + databaseName, username, pwd)) {
+            ps =
+                    conn.prepareStatement(
+                            "SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE TABLE_SCHEMA = ?;");
+            ps.setString(1, databaseName);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                tables.add(rs.getString(1));
+            }
+        } catch (Exception e) {
+            throw new CatalogException(
+                    String.format("Failed in listing database in catalog %s", getName()), e);
+        } finally {
+            release(rs);
+            release(ps);
+        }
+        return tables;
+    }
+
+    @Override
+    public CatalogBaseTable getTable(ObjectPath tablePath)
+            throws TableNotExistException, CatalogException {
+        if (!tableExists(tablePath)) {
+            throw new TableNotExistException(getName(), tablePath);
+        }
+
+        String dbUrl = baseUrl + tablePath.getDatabaseName();
+        try (Connection conn = DriverManager.getConnection(dbUrl, username, pwd)) {
+            DatabaseMetaData metaData = conn.getMetaData();
+
+            // MySQL 没有 schema 概念，直接传 null
+            Optional<UniqueConstraint> primaryKey =
+                    getPrimaryKey(metaData, null, tablePath.getObjectName());
+
+            PreparedStatement ps =
+                    conn.prepareStatement(
+                            String.format("SELECT * FROM %s;", tablePath.getObjectName()));
+
+            ResultSetMetaData rsmd = ps.getMetaData();
+
+            // 列名称
+            String[] names = new String[rsmd.getColumnCount()];
+            // 列类型(flink中)
+            DataType[] types = new DataType[rsmd.getColumnCount()];
+
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                names[i - 1] = rsmd.getColumnName(i);
+                types[i - 1] = fromJDBCType(rsmd, i);
+                if (rsmd.isNullable(i) == ResultSetMetaData.columnNoNulls) {
+                    types[i - 1] = types[i - 1].notNull();
+                }
+            }
+
+            TableSchema.Builder tableBuilder = new TableSchema.Builder().fields(names, types);
+            primaryKey.ifPresent(
+                    pk ->
+                            tableBuilder.primaryKey(
+                                    pk.getName(), pk.getColumns().toArray(new String[0])));
+
+            TableSchema tableSchema = tableBuilder.build();
+
+            Map<String, String> props = new HashMap<>();
+            props.put(CONNECTOR.key(), IDENTIFIER);
+            props.put(URL.key(), dbUrl);
+            props.put(TABLE_NAME.key(), tablePath.getObjectName());
+            props.put(USERNAME.key(), username);
+            props.put(PASSWORD.key(), pwd);
+            // 返回 CatalogTableImpl 与 create table sql 所做的事情是一致的
+            return new CatalogTableImpl(tableSchema, props, "");
+        } catch (Exception e) {
+            throw new CatalogException(
+                    String.format("Failed getting table %s", tablePath.getFullName()), e);
+        }
+    }
+
+    @Override
+    public boolean tableExists(ObjectPath tablePath) throws CatalogException {
+        List<String> tables = null;
+        try {
+            tables = listTables(tablePath.getDatabaseName());
+        } catch (DatabaseNotExistException e) {
+            return false;
+        }
+        return tables.contains(tablePath.getObjectName());
+    }
+
+    // Converts MySQL type to Flink {@link DataType}
+    private DataType fromJDBCType(ResultSetMetaData metadata, int colIndex) throws SQLException {
+        String mysqlType = metadata.getColumnTypeName(colIndex);
+
+        int precision = metadata.getPrecision(colIndex);
+        int scale = metadata.getScale(colIndex);
+
+        switch (mysqlType) {
+            case MYSQL_TINYINT:
+                if (1 == precision) {
+                    return DataTypes.BOOLEAN();
+                }
+                return DataTypes.TINYINT();
+            case MYSQL_SMALLINT:
+                return DataTypes.SMALLINT();
+            case MYSQL_TINYINT_UNSIGNED:
+                return DataTypes.SMALLINT();
+            case MYSQL_INT:
+                return DataTypes.INT();
+            case MYSQL_MEDIUMINT:
+                return DataTypes.INT();
+            case MYSQL_SMALLINT_UNSIGNED:
+                return DataTypes.INT();
+            case MYSQL_BIGINT:
+                return DataTypes.BIGINT();
+            case MYSQL_INT_UNSIGNED:
+                return DataTypes.BIGINT();
+            case MYSQL_BIGINT_UNSIGNED:
+                return DataTypes.DECIMAL(20, 0);
+            case MYSQL_FLOAT:
+                return DataTypes.FLOAT();
+            case MYSQL_DOUBLE:
+                return DataTypes.DOUBLE();
+            case MYSQL_NUMERIC:
+                return DataTypes.DECIMAL(precision, scale);
+            case MYSQL_DECIMAL:
+                return DataTypes.DECIMAL(precision, scale);
+            case MYSQL_BOOLEAN:
+                return DataTypes.BOOLEAN();
+            case MYSQL_DATE:
+                return DataTypes.DATE();
+            case MYSQL_TIME:
+                return DataTypes.TIME(scale);
+            case MYSQL_DATETIME:
+                return DataTypes.TIMESTAMP(scale);
+            case MYSQL_CHAR:
+                return DataTypes.CHAR(precision);
+            case MYSQL_VARCHAR:
+                return DataTypes.CHAR(precision);
+            case MYSQL_TEXT:
+                return DataTypes.STRING();
+            default:
+                throw new UnsupportedOperationException(
+                        String.format("Doesn't support mysql type '%s' yet", mysqlType));
+        }
+    }
+
+    private void release(AutoCloseable autoCloseable) {
+        if (Objects.nonNull(autoCloseable)) {
+            try {
+                autoCloseable.close();
+            } catch (Exception e) {
+                throw new CatalogException("Failed in releasing sql connection resource.", e);
+            }
+        }
+    }
+}
